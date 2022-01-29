f3bfbf3bbecec6fd000ffb0e3b3d7aef477cd824
John Ayling <john.ayling@digintent.com>
2022-01-29 09:04:03 -0600

    Add initial page routing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  view: string = 'recipe';
-
-  onViewSelected(view) {
-    this.view = view;
-  }
-}
+export class AppComponent { }
