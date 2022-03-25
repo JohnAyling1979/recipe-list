202df8314ed3517962dddb8fe17c211cbf71efd5
John Ayling <john.ayling@digintent.com>
2022-03-25 11:37:06 -0500

    Create shared module.

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-import { ReactiveFormsModule } from "@angular/forms";
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
@@ -10,6 +8,7 @@ import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.compo
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from "./recipes-routing.module";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
 	declarations: [
@@ -20,6 +19,6 @@ import { RecipesRoutingModule } from "./recipes-routing.module";
     RecipeStartComponent,
     RecipeEditComponent,
 	],
-	imports: [RouterModule, CommonModule, ReactiveFormsModule, RecipesRoutingModule],
+	imports: [RouterModule, SharedModule, RecipesRoutingModule],
 })
 export class RecipesModule {}
